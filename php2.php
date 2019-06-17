95afd924bc8843d2b01f8f0d1bb5ecf3980d220b
piyumishashikala <shashikalapiyumi9@gmail.com>
2019-06-17 11:52:47 +0530

    first commit

---
+++ php2.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Piyumi Shashikala
+ * Date: 6/17/2019
+ * Time: 9:38 AM
+ */?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<form action="webpage.php" method="post" id="nameform">
+    first name:<input type="text" name="frame"><br>
+last name:<input type="text" name="lrame"><br>
+    <button type="submit" form="nameform" value="submit">submit</button>
+    <button type="reset" value="reset">reset
+
+</form>
